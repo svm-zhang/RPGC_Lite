6543e58eb4c54f82c491f962d54bb02d9192d350
Simo Zhang <zhangsimo850114@gmail.com>
2013-08-22 12:00:25 -0400

    1. fix_redundant_calls.py v1.0.0 2. add argparse module and redesign command line arguments 3. add process_VCF() function and redesign how it works 4. fix a bug that some duplicates should be kept but not output to the output fixed VCF file

--- fix_redundant_calls.py
+++ fix_redundant_calls.py
@@ -1,65 +1,113 @@
 import os,sys
 import re
+from argparse import ArgumentParser
+from sys import exit, stdout, stderr
+from os import makedirs
+from os.path import exists, realpath, dirname
 
-final_vqsr_call = sys.argv[1]
-final_raw_call = sys.argv[2]
-out_fixed_file = sys.argv[3]
-fOUT = open(out_fixed_file, 'w')
+def process_VCF(input_vcf, out_dup_file, out_vcf = None) :
+	"""
+	identify duplicates and remove them if -fixoff is not specified
+	"""
 
-variants_dict = {}
-num_redundant = 0
-num_kept = 0
-fFINAL_VQSR = open(final_vqsr_call, 'r')
-for line in fFINAL_VQSR :
-	if line.startswith('#') :
-		if line.startswith("#CHROM") :
-			individuals = re.split('\t', line.strip())[9:]
-			print len(individuals), individuals
-	else :
-		tmp_line = re.split('\t', line.strip())
-		ref_base = tmp_line[3]
-		alt_base = tmp_line[4]
-		chrom_id = tmp_line[0]
-		chrom_pos = tmp_line[1]
-		qual = tmp_line[5]
-		filter = tmp_line[6]					# PASS or FILTERED by VQSR #
-		# fix sites having different types of calls: redundant calls #
-		if not variants_dict.has_key(chrom_id+':'+chrom_pos) :
-			variants_dict[chrom_id+':'+chrom_pos] = line.strip()
+	fVCF_OUT = None
+	if out_vcf is not None :
+		fVCF_OUT = open(out_vcf, 'w')
+	fDUP_OUT = open(out_dup_file, 'w')
+
+	variants_dict = {}
+	variants_list = []
+	num_redundant, num_kept = 0, 0
+	fINVCF = open(input_vcf, 'r')
+	for line in fINVCF :
+		if line.startswith('#') :
+			if line.startswith("#CHROM") :
+				individuals = re.split('\t', line.strip())[9:]
+				stdout.write("%d individuals included in the VCF file: %s\n" %(len(individuals), input_vcf))
+			if fVCF_OUT :
+				fVCF_OUT.write(line)
 		else :
-			num_redundant += 1
-			same_site_diff_call = re.split('\t', variants_dict[chrom_id+':'+chrom_pos])
-			print chrom_id, chrom_pos
-			tmp_qual = same_site_diff_call[5]
-			tmp_filter = same_site_diff_call[6]
-			tmp_alt_base = same_site_diff_call[4]
-			if (tmp_filter != "PASS" and filter != "PASS") or (filter == "PASS" and tmp_filter == "PASS") :		# if two different call both passed the VQSR or both not, we remove it from the final call set #	
-				variants_dict.pop(chrom_id+':'+chrom_pos)
-				if filter == "PASS" :
-					print chrom_id, chrom_pos+"both pass"
-				else :
-					print chrom_id, chrom_pos+"both filtered"
-			elif filter == "PASS" and tmp_filter != filter :
-				print chrom_id, chrom_pos + "second kept"
-				variants_dict.pop(chrom_id+':'+chrom_pos)
-				num_kept += 1
-				#variants_dict[chrom_id+':'+chrom_pos] = line.strip()
-			elif tmp_filter == "PASS" and tmp_filter != filter :
-				variants_dict.pop(chrom_id+':'+chrom_pos)
-				print chrom_id, chrom_pos + "first kept"
-				num_kept += 1
-print num_redundant, num_kept
-fFINAL_VQSR.close()
+			tmp_line = re.split('\t', line.strip())
+			ref_base = tmp_line[3]
+			alt_base = tmp_line[4]
+			chrom_id = tmp_line[0]
+			chrom_pos = tmp_line[1]
+			qual = tmp_line[5]
+			filter = tmp_line[6]					# PASS or FILTERED by VQSR #
+			# fix sites having different types of calls: redundant calls #
+			if not variants_dict.has_key(chrom_id+':'+chrom_pos) :
+				variants_dict[chrom_id+':'+chrom_pos] = line.strip()
+				variants_list.append(chrom_id+':'+chrom_pos)
+			else :
+				num_redundant += 1
+				same_site_diff_call = re.split('\t', variants_dict[chrom_id+':'+chrom_pos])
+				tmp_qual = same_site_diff_call[5]
+				tmp_filter = same_site_diff_call[6]
+				tmp_alt_base = same_site_diff_call[4]
+				fDUP_OUT.write("%s\n%s\n" %(variants_dict[chrom_id+':'+chrom_pos], line.strip()))
+				if (tmp_filter != "PASS" and filter != "PASS") or (filter == "PASS" and tmp_filter == "PASS") :		# if two different call both passed the VQSR or both not, we remove it from the final call set #	
+					variants_dict.pop(chrom_id+':'+chrom_pos)
+					variants_list.remove(chrom_id+':'+chrom_pos)
+					if filter == "PASS" :
+						stdout.write(chrom_id+" "+chrom_pos+" both pass\n")
+					else :
+						stdout.write(chrom_id+" "+chrom_pos+" both filtered\n")
+				elif filter == "PASS" and tmp_filter != filter :
+					stdout.write(chrom_id+" "+chrom_pos + " second kept\n")
+					variants_dict[chrom_id+':'+chrom_pos] = line.strip()
+					num_kept += 1
+				elif tmp_filter == "PASS" and tmp_filter != filter :
+					stdout.write(chrom_id+" "+chrom_pos + " first kept\n")
+					num_kept += 1
+	stdout.write("%d\t%d\n" %(num_redundant, num_kept))
 
-fFINAL_RAW = open(final_raw_call, 'r')
-for line in fFINAL_RAW :
-	if line.startswith("#") :
-		fOUT.write(line)
+	if fVCF_OUT :
+		for i in range(len(variants_list)) :
+			fVCF_OUT.write("%s\n" %(variants_dict[variants_list[i]]))
+		fVCF_OUT.close()
+	fINVCF.close()
+
+def time_stamper() :
+	"""
+	generate a dash-separated time stamped string
+	"""
+	return datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+
+def make_dir_if_needed(dir) :
+	""" Make a single directory if it does not exist. """
+	if not exists(dir) :
+		os.makedirs(dir)
+
+if __name__ == "__main__" :
+	parser = ArgumentParser(description="Identify and remove variants sites where two types of variants (SNP, INDEL) were identified.")
+	parser.add_argument("-vcf", metavar="FILE", dest="vcf_file", required=True, help="input VCF file")
+	parser.add_argument("-duplicates", metavar="FILE", dest="out_dup_file", required=True, help="output plain text with duplicated items identified, Whether or not -fixoff is specified, the program will spit out this file")
+	parser.add_argument("-out", metavar="FILE", dest="out_file", help="output VCF file with duplicates removed. Not required if -fixoff is specified")
+	parser.add_argument("-fixoff", action="store_true", dest="fix_or_not", help="turn off removing identified duplicates from input VCF file. No output VCF will be expected if this is specified")
+
+	args = parser.parse_args()
+
+	# file path sanity check #
+	if not exists(args.vcf_file) :
+		stderr.write(time_stamper() + " Error: cannot find the VCF file you provided: %s" %(args.vcf_file))
+		exit()
+
+	make_dir_if_needed(dirname(realpath(args.out_dup_file)))
+	out_dup_file = realpath(args.out_dup_file)
+
+	fix = -1
+	if args.fix_or_not :
+		fix = 0
 	else :
-		tmp_line = re.split('\t', line.strip())
-		chrom_id = tmp_line[0]
-		chrom_pos = tmp_line[1]
-		if variants_dict.has_key(chrom_id+':'+chrom_pos) :
-			fOUT.write(line)
-fOUT.close()
-fFINAL_RAW.close()
+		fix = 1
+
+	if fix :
+		if not args.out_file :
+			stderr.write(time_stamper() + " Error: you need to provide a path to the output VCF file with duplicate removed, since you specified -fix\n")
+			exit()
+		else :
+			make_dir_if_needed(dirname(realpath(args.out_file)))
+			out_file = realpath(args.out_file)
+			process_VCF(args.vcf_file, out_dup_file, out_file)
+	else :
+		process_VCF(args.vcf_file, out_dup_file)
