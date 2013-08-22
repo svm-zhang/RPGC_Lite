f8947d7a242f96c5d852e9828b9046c3d7631503
Simo Zhang <zhangsimo850114@gmail.com>
2013-08-22 17:02:45 -0400

    1. get_training_variants.py v1.0.0 2. add argparse module and redesign command line arguments 3. add functions and make code cleaner 4. -top_percent arugment might need to divided into -top_percent_snps and -top_percent_indels

--- get_training_variants.py
+++ get_training_variants.py
@@ -1,108 +1,112 @@
-import os, sys
 import re
+from argparse import ArgumentParser
+from os import makedirs
+from os.path import join, exists, dirname, realpath
+from sys import exit, stdout, stderr
+from datetime import datetime
 
-filtered_vcf_file = sys.argv[1]
-training_call_set_outprefix = sys.argv[2]
-top_percent_good_snps = int(sys.argv[3])
+def get_training_calls(vcf_file, outprefix_training_file, top_percent) :
+	training_snps_outfile = outprefix_training_file + ".snp.vcf"
+	training_indels_outfile = outprefix_training_file + ".indels.vcf"
+	fTRAIN_SNP = open(training_snps_outfile, 'w')
+	fTRAIN_INDEL = open(training_indels_outfile, 'w')
 
-training_snps_outfile = training_call_set_outprefix + ".snps.train_1.vcf"
-training_indels_outfile = training_call_set_outprefix + ".indels.train_1.vcf"
-fTRAIN_SNP = open(training_snps_outfile, 'w')
-fTRAIN_INDEL = open(training_indels_outfile, 'w')
-
-variants_dict = {}
-snps_call_score_dict = {}
-indels_call_score_dict = {}
-num_variants, num_snps, num_indels = 0, 0, 0
-headers = ""
-fFVCF = open(filtered_vcf_file, 'r')
-for line in fFVCF :
-	if re.match("^#", line.strip()) :
-		if line.startswith("#CHROM") :
-			individuals = re.split('\t', line.strip())[9:]
-			print len(individuals)
-		fTRAIN_SNP.write(line)
-		fTRAIN_INDEL.write(line)
-	else :
-		tmp_line = re.split('\t', line.strip())
-		chrom_id = tmp_line[0]
-		chrom_pos = tmp_line[1]
-		ref_base = tmp_line[3]
-		alt_base = tmp_line[4]
-		variants_dict[chrom_id+":"+chrom_pos] = line.strip()
-		qual = float(tmp_line[5])
-		if re.findall("DP=\d+", tmp_line[7]) :
-			dp = re.findall("DP=\d+", tmp_line[7])[0]
-			if int(re.split('=', dp)[1]) >= 40 and int(re.split('=', dp)[1]) <= 60 :
-				if len(re.split(',', alt_base)) == 1 :
-					########
-					# SNPs #
-					########
-					if len(ref_base) == len(alt_base) and len(ref_base) == 1 :
-						num_het, num_homo_nonRef, num_homo_Ref = 0, 0, 0
-						tmp_num_individual = len(individuals)
-						for i in range(9, len(tmp_line)) :
-							if re.split(':', tmp_line[i])[0] == "1/1" and int(re.split(':', tmp_line[i])[2]) >= 4 :
-								num_homo_nonRef += 1
-							#if re.split(':', tmp_line[i])[0] == "0/0" and int(re.split(':', tmp_line[i])[2]) >= 4 :
-							#	num_homo_Ref += 1
-							#if re.split(':', tmp_line[i])[0] == "./." :
-							#	tmp_num_individual -= 1
-							if re.split(':', tmp_line[i])[0] == "0/1" :
-								num_het += 1
-						if num_homo_nonRef >= 1 and num_het <= 1 :
-							#fTRAIN_SNP.write(line)
+	variants_dict = {}
+	snps_call_score_dict = {}
+	indels_call_score_dict = {}
+	num_variants, num_snps, num_indels = 0, 0, 0
+	headers = ""
+	stdout.write("Parsing the VCF file: %s\n" %(vcf_file))
+	fVCF = open(vcf_file, 'r')
+	for line in fVCF :
+		if line.startswith("#") :
+			if line.startswith("#CHROM") :
+				individuals = re.split('\t', line.strip())[9:]
+			fTRAIN_SNP.write(line)
+			fTRAIN_INDEL.write(line)
+		else :
+			tmp_line = re.split('\t', line.strip())
+			chrom_id = tmp_line[0]
+			chrom_pos = tmp_line[1]
+			ref_base = tmp_line[3]
+			alt_base = tmp_line[4]
+			variants_dict[chrom_id+":"+chrom_pos] = line.strip()
+			qual = float(tmp_line[5])
+			if re.findall("DP=\d+", tmp_line[7]) :
+				dp = re.findall("DP=\d+", tmp_line[7])[0]
+				if int(re.split('=', dp)[1]) >= 40 and int(re.split('=', dp)[1]) <= 60 :
+					if len(re.split(',', alt_base)) == 1 :
+						########
+						# SNPs #
+						########
+						if len(ref_base) == len(alt_base) and len(ref_base) == 1 :
 							if not snps_call_score_dict.has_key(qual) :
 								snps_call_score_dict[qual] = [chrom_id+":"+chrom_pos]
 							else :
 								snps_call_score_dict[qual].append(chrom_id+":"+chrom_pos)
 							num_snps += 1
-					##########
-					# INDELs #
-					##########
-					elif len(ref_base) > len(alt_base) or len(ref_base) < len(alt_base) :
-						num_het, num_homo_nonRef = 0, 0
-						for i in range(9, len(tmp_line)) :
-							if re.split(':', tmp_line[i])[0] == "1/1" and int(re.split(':', tmp_line[i])[2]) >= 4 :
-								num_homo_nonRef += 1
-							if re.split(':', tmp_line[i])[0] == "0/1" :
-								num_het += 1
-						if num_homo_nonRef >= 1 and num_het <= 1 :
+						##########
+						# INDELs #
+						##########
+						elif len(ref_base) > len(alt_base) or len(ref_base) < len(alt_base) :
 							fTRAIN_INDEL.write(line)
 							if not indels_call_score_dict.has_key(qual) :
 								indels_call_score_dict[qual] = [chrom_id+":"+chrom_pos]
 							else :
 								indels_call_score_dict[qual].append(chrom_id+":"+chrom_pos)
 							num_indels += 1
-		num_variants += 1
-fTRAIN_INDEL.close()
-print num_variants
-print num_snps, num_indels
+			num_variants += 1
+	fTRAIN_INDEL.close()
+	stdout.write("%d variants in %s\n" %(num_variants, vcf_file))
+	stdout.write("\t%d SNPs parsed out\n" %(num_snps))
+	stdout.write("\t%d INDELS parsed out\n" %(num_indels))
 
-num_high_scoring_calls = 0.0
-high_number_calls = []
-for score in sorted(snps_call_score_dict.iterkeys(), reverse=True) :
-	#if num_high_scoring_calls == 0.0 :
-	#	print snps_call_score_dict[score]
-	for i in range(len(snps_call_score_dict[score])) :
-		num_high_scoring_calls += 1
-		if num_high_scoring_calls/num_snps <= (top_percent_good_snps/100.0) :
-			high_number_calls.append(snps_call_score_dict[score][i])
+	stdout.write("Output the top %d%% of the variants\n" %(top_percent))
+	num_high_scoring_calls = 0.0
+	high_number_calls = []
+	for score in sorted(snps_call_score_dict.iterkeys(), reverse=True) :
+		for i in range(len(snps_call_score_dict[score])) :
+			num_high_scoring_calls += 1
+			if num_high_scoring_calls/num_snps <= (top_percent/100.0) :
+				high_number_calls.append(snps_call_score_dict[score][i])
 
-print len(high_number_calls)
+	tmp_high_number_calls = high_number_calls
+	fVCF.seek(0)
+	for line in fVCF :
+		if not line.startswith("#") :
+			tmp_line = re.split('\t', line.strip())
+			chrom_id = tmp_line[0]
+			chrom_pos = tmp_line[1]
+			
+			if chrom_id+":"+chrom_pos in tmp_high_number_calls :
+				fTRAIN_SNP.write(line)
+				tmp_high_number_calls.pop(tmp_high_number_calls.index(chrom_id+':'+chrom_pos))
+	fVCF.close()
+	fTRAIN_SNP.close()
 
-tmp_high_number_calls = high_number_calls
-fFVCF.seek(0)
-for line in fFVCF :
-	if re.match("^#", line.strip()) :
-		continue
-	else :
-		tmp_line = re.split('\t', line.strip())
-		chrom_id = tmp_line[0]
-		chrom_pos = tmp_line[1]
-		
-		if chrom_id+":"+chrom_pos in tmp_high_number_calls :
-			fTRAIN_SNP.write(line)
-			tmp_high_number_calls.pop(tmp_high_number_calls.index(chrom_id+':'+chrom_pos))
-fFVCF.close()
-fTRAIN_SNP.close()
+def time_stamper() :
+	"""
+	generate a dash-separated time stamped string
+	"""
+	return datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+
+def make_dir_if_needed(dir) :
+	""" Make a single directory if it does not exist. """
+	if not exists(dir) :
+		os.makedirs(dir)
+
+if __name__ == "__main__" :
+	parser = ArgumentParser(description="Get the top X percent of the highest-scoring calls and use it as the input to GATK VQSR")
+	parser.add_argument("-vcf", metavar="FILE", required=True, dest="vcf_file", help="input VCF file obtained from variant_calls_filter.py")
+	parser.add_argument("-out", metavar="PREFIX", required=True, dest="outprefix_training_file", help="output prefix of VCF file used for training")
+	parser.add_argument("-percent", metavar="INT", dest="top_percent", type=int, default=10,  help="user specified top X percent of the hightest-scoring calls")
+
+	args = parser.parse_args()
+
+	if not exists(realpath(args.vcf_file)) :
+		stderr.write(time_stamper() + " Error: cannot find the file you provided: %s" %(realpath(args.vcf_file)))
+		exit()
+
+	make_dir_if_needed(dirname(realpath(args.outprefix_training_file)))
+
+	get_training_calls(realpath(args.vcf_file), realpath(args.outprefix_training_file), args.top_percent)
